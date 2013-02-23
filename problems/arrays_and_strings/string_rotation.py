61a9b22e16efd9483598c43bc92db5796719527d
Jim Holmström <jim.holmstroem@gmail.com>
2013-02-23 17:08:33 +0100

    really simple solution overdid it a bit at first, sometimes you have to make things bigger to make them easily solveble

---
+++ problems/arrays_and_strings/string_rotation.py
@@ -0,0 +1,33 @@
+
+import itertools as it
+
+#check if a two strings is rotation of each other. 
+#ex: "hello world" and "llo worldhe"
+
+# isSubstring: "ll" in "hello"
+# isSubstring: "hello".find("ll")!=-1
+
+def isRotation(s1, s2):
+    #you have one unknown variable and that is the 'start' position
+    #trivial search would be O(N*string_equal) = O(N^2)
+
+    #if both words are the same length and you double word A then B in AA
+    #if this is true it exists an rotated offset between them and they are thus 
+    #guarenteed to be rotations of each other
+    #this is O(N)
+
+    return len(s1)==len(s2) and s1 in s2*2
+
+print(all(
+    (
+        isRotation("hello","elloh"),
+        not isRotation("hbllo","elloh"),
+        isRotation("heheo","eheoh"),
+        not isRotation("hello","h"),
+        not isRotation("h","elloh"),
+        isRotation("bobobo","obobob"),
+        isRotation("saxxsa","sasaxx"),
+        isRotation("s","s"),
+        isRotation("",""),
+    )
+))

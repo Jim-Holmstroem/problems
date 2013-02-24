1bd9ed70cd821311b90758e24a0db2070eddfc39
Jim Holmström <jim.holmstroem@gmail.com>
2013-02-24 11:29:46 +0100

    smarter way to get the bits out of the double, forgot about the longlong (what was I thinking)

--- problems/bit_manipulation/double.cpp
+++ problems/bit_manipulation/double.cpp
@@ -7,6 +7,7 @@
 union u_double
 {
     double dbl;
+    long long lng;
     char data[sizeof(double)];
 };
 
@@ -14,42 +15,48 @@ void print_32bit_double(double value)
 {
     union u_double d;
     d.dbl = value;
-    //for(int i=LEN(d.data); i-->0;) //little endian
-    //{
-    //    std::cout << std::bitset<8>(d.data[i]) << " ";
-    //}
-    //std::cout << std::endl;
 
-    //any bits outside the 32bits
-    if(std::bitset<4>(d.data[5]).any() /*the lower 4bits*/ || std::bitset<8>(d.data[6]).any() || std::bitset<8>(d.data[7]).any())
-    {
-        std::cout << "ERROR" << std::endl;
-    }
-    std::cout << "0.";
+    std::cout << std::bitset<1>(d.lng>>63);
+    std::cout << "|" << std::bitset<11>(d.lng>>52);
+    std::cout << "|" << std::bitset<52>(d.lng) << std::endl;
 
-    //inside the 32bits
-    std::cout << std::bitset<4>(d.data[1]);
-    std::cout << std::bitset<8>(d.data[2]);
-    std::cout << std::bitset<8>(d.data[3]);
-    std::cout << std::bitset<8>(d.data[4]);
-    std::cout << std::bitset<4>(d.data[5]>>4); //shift down the higher 4bits
-    std::cout << std::endl;
 };
 
 int main()
 {
-    std::cout << std::bitset<4>(0).none() << std::endl;
+    print_32bit_double(-8.0);
+    print_32bit_double(-7.5);
+    print_32bit_double(-7.0);
+    print_32bit_double(-6.5);
+    print_32bit_double(-6.0);
+    print_32bit_double(-5.5);
+    print_32bit_double(-5.0);
+    print_32bit_double(-4.5);
+    print_32bit_double(-4.0);
+    print_32bit_double(-3.5);
+    print_32bit_double(-3.0);
     print_32bit_double(-2.5);
     print_32bit_double(-2.0);
     print_32bit_double(-1.5);
     print_32bit_double(-1.0);
     print_32bit_double(-0.5);
-    print_32bit_double( 0.0);
+    print_32bit_double( 0.0); //explicit to avoid calculation roundoffs
     print_32bit_double( 0.5);
     print_32bit_double( 1.0);
     print_32bit_double( 1.5);
     print_32bit_double( 2.0);
     print_32bit_double( 2.5);
+    print_32bit_double( 3.0);
+    print_32bit_double( 3.5);
+    print_32bit_double( 4.0);
+    print_32bit_double( 4.5);
+    print_32bit_double( 5.0);
+    print_32bit_double( 5.5);
+    print_32bit_double( 6.0);
+    print_32bit_double( 6.5);
+    print_32bit_double( 7.0);
+    print_32bit_double( 7.5);
+    print_32bit_double( 8.0);
     std::cout << "===========" <<std::endl;
     print_32bit_double(-0.1);
     print_32bit_double( 0.1);

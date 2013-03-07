7534f9e087c06a272cd88d6089e34f0c03bad82b
Jim Holmström <jim.holmstroem@gmail.com>
2013-03-07 23:43:29 +0100

    added description to bitdiff

--- problems/bit_manipulation/bitdiff.py
+++ problems/bit_manipulation/bitdiff.py
@@ -1,6 +1,7 @@
 from utils import bitcount
 
 def bitdiff(a, b):
+    #count the number of bit flips required to go from a to b
     return bitcount(a^b) #count the bits that differs
 
 testcases = [

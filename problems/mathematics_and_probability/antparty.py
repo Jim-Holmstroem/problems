36856d6e87cca4e50306c0364f06a98ed24f0c01
Jim Holmström <jim.holmstroem@gmail.com>
2013-03-08 23:07:48 +0100

    :0

---
+++ problems/mathematics_and_probability/antparty.py
@@ -0,0 +1,11 @@
+
+#
+# What is the probability that two ants collide when from one verex to another in
+# an n-gon?
+#
+# a priori p=1/2 (right or left)
+#
+# there is only a non-collision if all ants chooses the same direction
+# p(collision) = 1 - p(non-collision) = 1-p_left^n-p_right^n = 1-(1/2)^n 
+#
+# specially for n=3 (i.e triangle) = 1 - 2(1/2)^3 = 1-2/8 = 3/4

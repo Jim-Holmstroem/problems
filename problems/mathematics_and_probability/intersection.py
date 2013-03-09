1c08455eabfc220acad335abb8db40c2f422f822
Jim Holmström <jim.holmstroem@gmail.com>
2013-03-09 22:35:08 +0100

    fixed the last part of the intersection as well as a few specialcases, also a few principal tests, but it would really need some unit testing for some of the inner methods

--- problems/mathematics_and_probability/intersection.py
+++ problems/mathematics_and_probability/intersection.py
@@ -33,6 +33,10 @@
 #  that is, is the intersection with each lines boundingbox given their own parameter?
 #
 
+import functools as ft
+import itertools as it
+import operator as op
+
 class point(object):
     def __init__(self, x, y):
         self.x = x
@@ -49,7 +53,7 @@ class point(object):
     def __str__(self):
         return "({x}, {y})".format(x=self.x, y=self.y)
     __repr__ = __str__ 
-
+ 
 class line(object):
     """ starts at pa and goes to pb"""
     def __init__(self, pa, pb):
@@ -57,19 +61,101 @@ class line(object):
         self.pb = pb
         self.delta = pb-pa
     def __call__(self, t):
+        """the actual line is in t in [0,1] else outside i.e the infinite extension of the line"""
         return self.pa + self.delta*t
     def __str__(self):
         return "[{pa}, {pb}]".format(pa=self.pa, pb=self.pb)
+    def __contains__(self, t):
+        return 0<=t and t<=1 #closed set, T is considered as an intersection
     __repr__ = __str__
 
 def intersect(line1, line2):
     det = (line1.delta.x*line2.delta.y - line2.delta.x*line1.delta.y)
-    dp0 = line1.pa - line2.pa #delta p0
-    s, t = (
-        float(-line2.delta.y*dp0.x + line2.delta.x*dp0.y)/det,
-        float(-line1.delta.y*dp0.x + line1.delta.x*dp0.y)/det,
-    )
-    print(line1(s))
-    print(line2(t))
+    if(det):
+        dp0 = line1.pa - line2.pa #delta p0
+        s, t = (
+            float(-line2.delta.y*dp0.x + line2.delta.x*dp0.y)/det,
+            float(-line1.delta.y*dp0.x + line1.delta.x*dp0.y)/det,
+        )
+        print("{0}=={1}".format(line1(s), line2(t))) #consistent collision
+        return (s in line1) and (t in line2)
+    else:
+        def inline(line1, line2):
+            """check if two vectors conincide to see if they are inline"""
+            v1 = line1.pa - line1.pb #two vectors with line1.pa as reference
+            v2 = line1.pa - line2.pb #
+            det = v1.x*v2.y-v1.y*v2.x
+            return det==0.0 #little risky comparison but python mostly handles it well, they are actually only in line if this is true. but it might be possible to construction parallel inlines for which this returns false by roundoff errors.
+    
+        def overlapping(line1, line2):
+            """ should handle verticle lines, some are tricky"""
+            def inside(p, l, component):
+                ps = map(component, [l.pa, l.pb])
+                return min(*ps) <= component(p) and component(p) <= max(*ps)
+            insidex = ft.partial(inside, component=op.attrgetter('x'))
+            insidey = ft.partial(inside, component=op.attrgetter('y'))
 
-intersect(line(point(0,0),point(1,1)),line(point(0,1),point(1,0))) #intersect in (0.5,0.5)
+            xwise, ywise = (
+                sum(
+                    it.starmap(
+                        insidex,
+                        it.chain(
+                            it.product(
+                                [line1.pa, line1.pb],
+                                [line2,]
+                            ),
+                            it.product(
+                                [line2.pa, line2.pb],
+                                [line1,]
+                            )
+                        )
+                    )
+                ),
+                sum(
+                    it.starmap(
+                        insidey,
+                        it.chain(
+                            it.product(
+                                [line1.pa, line1.pb],
+                                [line2,]
+                            ),
+                            it.product(
+                                [line2.pa, line2.pb],
+                                [line1,]
+                            )
+                        )
+                    )
+                )
+            )
+            assert(xwise==2 or xwise==0)
+            assert(ywise==2 or ywise==0)
+            return xwise==2 and ywise==2
+
+        return inline(line1, line2) and overlapping(line1, line2)
+
+print(intersect(
+    line(point(  0,  0), point(  1,  1)), 
+    line(point(  0,  1), point(  1,  0))
+)) #intersect in (0.5,0.5)
+print(intersect(
+    line(point(0.5,  0), point(0.5,  1)), 
+    line(point(  0,0.5), point(  1,0.5))
+)) #intersect in (0.5,0.5)
+print(not intersect(
+    line(point(  0,  0), point(  0,  1)), 
+    line(point(  1,  0), point(  1,  1))
+)) #no intersection since parallel, and not in line
+print(intersect(
+    line(point(  0,  0), point(  1,  1)), 
+    line(point(0.5,0.5), point(  2,  2))
+)) #intersection since parallel and has common points 
+print(intersect(
+    line(point(  0,  0), point(  0,  1)), 
+    line(point(  0,0.5), point(  1,0.5))
+)) #T intersection
+print(not intersect(
+    line(point(0.5,  0), point(0.5,  1)), 
+    line(point( 10,0.5), point( 11,0.5))
+)) #no intersection
+
+#whould need more tests, aspecially all specialcases, and perhaps doesn't handle lines with length of zero
